1756462eb5c8f3d072ddd4a77d928346814d2c5d
lsy <lsy@skynet.com>
2021-01-18 19:00:55 +0800

    左右结构表格框架搭建完成

--- src/pages/table/tabs/index.jsx
+++ src/pages/table/tabs/index.jsx
@@ -1,85 +1,180 @@
-/* import { Divider } from 'antd'; */
-import React /* , { Fragment } */ from 'react';
-import { PageContainer } from '@ant-design/pro-layout';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { Button, Badge } from 'antd';
 import ProTable from '@ant-design/pro-table';
-import { selectStationValue } from '@/utils/dictionary';
-import { queryCar } from './service/index';
+import ProCard from '@ant-design/pro-card';
+import styles from './styles/index.less';
 
-const ActionTable = () => {
+const tableIp = (item) => <Badge status={item.status} text={item.ip} />;
+
+const DetailList = (props) => {
+  const { ip } = props;
+  const [tableListDataSource, setTableListDataSource] = useState([]);
   const columns = [
     {
-      title: '单位名称',
-      dataIndex: 'name',
-      valueType: 'input',
-    },
-    {
-      title: '单位类型',
-      dataIndex: 'type',
-      search: false,
-    },
-    {
-      title: '单位地址',
-      dataIndex: 'address',
-      valueType: 'input',
-    },
-    {
-      title: '采集时间',
-      dataIndex: 'time',
-      search: false,
-    },
-    {
-      title: '采集派出所',
-      dataIndex: 'station',
-      valueType: 'select',
-      valueEnum: selectStationValue,
-    },
-    {
-      title: '采集人',
-      dataIndex: 'collectionPeople',
-      search: false,
-    },
-    {
-      title: '采集开始时间',
-      dataIndex: 'collectionStartTime',
-      hideInTable: true,
+      title: '时间点',
+      key: 'createdAt',
+      dataIndex: 'createdAt',
       valueType: 'dateTime',
     },
     {
-      title: '采集结束时间',
-      dataIndex: 'collectionEndTime',
-      hideInTable: true,
-      valueType: 'dateTime',
+      title: '代码',
+      key: 'code',
+      width: 80,
+      dataIndex: 'code',
+      valueType: 'code',
     },
-    /* {
+    {
       title: '操作',
-      dataIndex: 'option',
+      key: 'option',
+      width: 80,
       valueType: 'option',
-      render: () => (
-        <Fragment>
-          <a>操作</a>
-          <Divider type="vertical" />
-          <a>操作</a>
-        </Fragment>
-      ),
-    }, */
+      render: () => [<a key="a">预警</a>],
+    },
   ];
+  useEffect(() => {
+    const source = [];
+    for (let i = 0; i < 15; i += 1) {
+      source.push({
+        createdAt: Date.now() - Math.floor(Math.random() * 10000),
+        code: `const getData = async params => {
+          const data = await getData(params);
+          return { list: data.data, ...data };
+        };`,
+        key: i,
+      });
+    }
+    setTableListDataSource(source);
+  }, [ip]);
   return (
-    <PageContainer>
-      <ProTable
-        rowKey="id"
-        search={{
-          labelWidth: 120,
-        }}
-        bordered
-        pagination={{
-          showQuickJumper: true,
-        }}
-        headerTitle="暂未开发"
-        request={(params) => queryCar({ ...params })}
-        columns={columns}
-      />
-    </PageContainer>
+    <ProTable
+      columns={columns}
+      dataSource={tableListDataSource}
+      pagination={{
+        pageSize: 5,
+        showSizeChanger: false,
+      }}
+      rowKey="key"
+      toolBarRender={false}
+      search={false}
+    />
   );
 };
 
-export default ActionTable;
+DetailList.propTypes = {
+  ip: PropTypes.any,
+};
+
+const valueEnum = ['success', 'error', 'processing', 'default'];
+const ipListDataSource = [];
+for (let i = 0; i < 20; i += 1) {
+  ipListDataSource.push({
+    ip: `106.14.98.1${i}4`,
+    cpu: 10,
+    mem: 20,
+    status: valueEnum[Math.floor(Math.random() * 10) % 4],
+    disk: 30,
+  });
+}
+const IPList = (props) => {
+  const { onChange, ip } = props;
+  const columns = [
+    {
+      title: 'IP',
+      key: 'ip',
+      dataIndex: 'ip',
+      render: (_, item) => tableIp(item),
+    },
+    {
+      title: 'CPU',
+      key: 'cpu',
+      dataIndex: 'cpu',
+      valueType: {
+        type: 'percent',
+        precision: 0,
+      },
+    },
+    {
+      title: 'Mem',
+      key: 'mem',
+      dataIndex: 'mem',
+      valueType: {
+        type: 'percent',
+        precision: 0,
+      },
+    },
+    {
+      title: 'Disk',
+      key: 'disk',
+      dataIndex: 'disk',
+      valueType: {
+        type: 'percent',
+        precision: 0,
+      },
+    },
+  ];
+  return (
+    <ProTable
+      columns={columns}
+      request={(params, sorter, filter) => {
+        // 表单搜索项会从 params 传入，传递给后端接口。
+        console.log(params, sorter, filter);
+        return Promise.resolve({
+          data: ipListDataSource,
+          success: true,
+        });
+      }}
+      rowKey="ip"
+      rowClassName={(record) => {
+        return record.ip === ip ? styles['split-row-select-active'] : '';
+      }}
+      toolbar={{
+        search: {
+          onSearch: (value) => {
+            alert(value);
+          },
+        },
+        actions: [
+          <Button key="list" type="primary">
+            新建项目
+          </Button>,
+        ],
+      }}
+      options={false}
+      pagination={{
+        pageSize: 15,
+        showSizeChanger: false,
+      }}
+      search={false}
+      onRow={(record) => {
+        return {
+          onClick: () => {
+            if (record.ip) {
+              onChange(record.ip);
+            }
+          },
+        };
+      }}
+    />
+  );
+};
+
+IPList.propTypes = {
+  onChange: PropTypes.any,
+  ip: PropTypes.any,
+};
+
+const TabsTable = () => {
+  const [ip, setIp] = useState('0.0.0.0');
+  return (
+    <ProCard split="vertical">
+      <ProCard colSpan="384px" ghost>
+        <IPList onChange={(cIp) => setIp(cIp)} ip={ip} />
+      </ProCard>
+      <ProCard title={ip}>
+        <DetailList ip={ip} />
+      </ProCard>
+    </ProCard>
+  );
+};
+export default TabsTable;
