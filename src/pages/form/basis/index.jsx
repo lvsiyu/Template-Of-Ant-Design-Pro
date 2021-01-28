5c9de5256bb4d73dbce14d1ea6a26474195347ef
lvsiyu <1030502996@qq.com>
2021-01-28 15:50:04 +0800

    '基础表单样式调整'

--- src/pages/form/basis/index.jsx
+++ src/pages/form/basis/index.jsx
@@ -6,12 +6,14 @@ import { FooterToolbar, PageContainer } from '@ant-design/pro-layout';
 import ProCard from '@ant-design/pro-card';
 import ProForm, {
   ProFormText,
+  ProFormDatePicker,
   ProFormDateRangePicker,
   ProFormSelect,
   ProFormRadio,
   ProFormUploadButton,
   ProFormDigit,
   ProFormTextArea,
+  ProFormCheckbox,
 } from '@ant-design/pro-form';
 
 const waitTime = (time = 100) => {
@@ -45,103 +47,96 @@ const BasisForm = (props) => {
 
   return (
     <PageContainer>
-      <ProCard>
-        <ProForm
-          submitter={{
-            render: (_, dom) => basisFormFooter(_, dom),
-          }}
-          onFinish={async (values) => {
-            await waitTime(2000);
-            submitForm(values);
-          }}
-        >
-          <ProForm.Group>
-            <ProFormText
-              name="text1"
-              label="限制长度的输入框"
-              tooltip="最长为 24 位"
-              placeholder="请输入"
-              rules={[{ required: true, message: '请输入内容!' }]}
+      <ProForm
+        submitter={{
+          render: (_, dom) => basisFormFooter(_, dom),
+        }}
+        onFinish={async (values) => {
+          await waitTime(2000);
+          submitForm(values);
+        }}
+      >
+        <ProCard gutter={[{ xs: 8, sm: 8, md: 16, lg: 24, xl: 32 }, 16]}>
+          <ProCard bordered headerBordered title="输入框">
+            <ProForm.Group>
+              <ProFormText
+                name="text1"
+                label="默认长度的输入框"
+                tooltip="最长为 24 位"
+                placeholder="请输入"
+                rules={[{ required: true, message: '请输入内容!' }]}
+              />
+            </ProForm.Group>
+            <ProForm.Group>
+              <ProFormText width="xs" name="text2" label="104长度的输入框" placeholder="请输入" />
+            </ProForm.Group>
+            <ProForm.Group>
+              <ProFormText width="sm" name="text3" label="216长度的输入框" placeholder="请输入" />
+            </ProForm.Group>
+            <ProForm.Group>
+              <ProFormText width="md" name="text4" label="328长度的输入框" placeholder="请输入" />
+            </ProForm.Group>
+            <ProForm.Group>
+              <ProFormText width="lg" name="text5" label="440长度的输入框" placeholder="请输入" />
+            </ProForm.Group>
+            <ProForm.Group>
+              <ProFormText width="xl" name="text6" label="552长度的输入框" placeholder="请输入" />
+            </ProForm.Group>
+            <ProForm.Group>
+              <ProFormDigit width="md" name="num" label="数字输入框" initialValue={5} />
+            </ProForm.Group>
+            <ProFormText name="text7" label="未限制长度的输入框" placeholder="请输入" />
+          </ProCard>
+          <ProCard bordered headerBordered title="选择框">
+            <ProForm.Group>
+              <ProFormDatePicker width="md" name="datepicker" label="日期选择框" />
+            </ProForm.Group>
+            <ProForm.Group>
+              <ProFormDateRangePicker width="md" name="rangepicker" label="日期范围选择框" />
+            </ProForm.Group>
+            <ProForm.Group>
+              <ProFormSelect
+                options={[{ value: 'chapter', label: '选项1' }]}
+                width="md"
+                name="select"
+                label="下拉选择框"
+              />
+            </ProForm.Group>
+
+            <ProForm.Group>
+              <ProFormSelect
+                initialValue="option1"
+                options={[{ value: 'option1', label: '选项1' }]}
+                width="md"
+                name="initialselect"
+                label="带默认值的下拉选择框"
+              />
+            </ProForm.Group>
+            <ProForm.Group>
+              <ProFormRadio.Group
+                label="单选框"
+                name="radio"
+                initialValue="单选1"
+                options={['单选1', '单选2', '单选3']}
+              />
+            </ProForm.Group>
+            <ProForm.Group>
+              <ProFormCheckbox.Group
+                label="多选框"
+                name="checkbox"
+                options={['多选1', '多选2', '多选3']}
+              />
+            </ProForm.Group>
+            <ProFormUploadButton
+              extra="支持扩展名：.jpg .zip .doc .wps"
+              label="倒签报备附件"
+              name="file"
+              title="上传文件"
             />
-            <ProFormText width="md" name="text2" label="未限制长度的输入框" placeholder="请输入" />
-          </ProForm.Group>
-          <ProForm.Group>
-            <ProFormText name="text3" label="默认长度的输入框" placeholder="请输入" />
-            <ProFormDateRangePicker width="md" name="rangepicker" label="时间范围选择框" />
-          </ProForm.Group>
-          <ProForm.Group>
-            <ProFormSelect
-              options={[
-                {
-                  value: 'chapter',
-                  label: '盖章后生效',
-                },
-              ]}
-              width="xs"
-              name="chapter"
-              label="合同约定生效方式"
-            />
-            <ProFormSelect
-              width="xs"
-              options={[
-                {
-                  value: 'time',
-                  label: '履行完终止',
-                },
-              ]}
-              name="unusedMode"
-              label="合同约定失效效方式"
-            />
-          </ProForm.Group>
-          <ProFormText width="sm" name="id" label="主合同编号" />
-          <ProFormText name="project" disabled label="项目名称" initialValue="xxxx项目" />
-          <ProFormText width="xs" name="mangerName" disabled label="商务经理" initialValue="启途" />
-          <ProForm.Group>
-            <ProFormSelect
-              initialValue="money"
-              options={[
-                {
-                  value: 'money',
-                  label: '确认金额',
-                },
-              ]}
-              width="xs"
-              name="useMode"
-              label="金额类型"
-            />
-            <ProFormSelect
-              options={[
-                {
-                  value: '6',
-                  label: '6%',
-                },
-                {
-                  value: '12',
-                  label: '12%',
-                },
-              ]}
-              initialValue="6"
-              width="xs"
-              name="taxRate"
-              label="税率"
-            />
-            <ProFormRadio.Group
-              label="发票类型"
-              name="invoiceType"
-              initialValue="发票"
-              options={['发票', '普票', '无票']}
-            />
-          </ProForm.Group>
-          <ProFormUploadButton
-            extra="支持扩展名：.jpg .zip .doc .wps"
-            label="倒签报备附件"
-            name="file"
-            title="上传文件"
-          />
-          <ProFormDigit width="xs" name="num" label="合同份数" initialValue={5} />
-          <ProFormTextArea width="xl" label="合同备注说明" name="remark" />
-        </ProForm>
-      </ProCard>
+            <ProFormTextArea width="xl" label="合同备注说明" name="remark" />
+          </ProCard>
+        </ProCard>
+      </ProForm>
     </PageContainer>
   );
 };
