c89461eede518bbc46e23707f4a305680cbbecb5
lvsiyu <1030502996@qq.com>
2021-02-03 16:30:45 +0800

    '基础详情添加骨架屏'

--- src/pages/detail/basis/index.jsx
+++ src/pages/detail/basis/index.jsx
@@ -1,41 +1,52 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'umi';
 import { Card, Divider } from 'antd';
 import { PageContainer } from '@ant-design/pro-layout';
+import ProSkeleton from '@ant-design/pro-skeleton';
 import { BasisDetail1, BasisDetail2, DetailList1, DetailList2 } from './components/index';
 import styles from './style/index.less';
+
+const waitTime = (time = 100) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, time);
+  });
+};
 
 const BasisDetail = (props) => {
   const { dispatch, basisDetailData, loading } = props;
 
   useEffect(() => {
-    if (dispatch) {
-      dispatch({
-        type: 'basisDetail/queryBasisDetail',
-      });
+    async function fetchData() {
+      await waitTime(2000);
+      if (dispatch) {
+        dispatch({
+          type: 'basisDetail/queryBasisDetail',
+        });
+      }
     }
+    fetchData();
   }, [dispatch]);
 
   return (
     <PageContainer>
       <Card bordered={false}>
-        {basisDetailData && basisDetailData.basisDescriptionData1 ? (
-          <BasisDetail1 data={basisDetailData.basisDescriptionData1} loading={loading} />
-        ) : null}
-        <Divider style={{ marginBottom: 32 }} />
-        {basisDetailData && basisDetailData.basisDescriptionData2 ? (
-          <BasisDetail2 data={basisDetailData.basisDescriptionData2} loading={loading} />
-        ) : null}
-        <Divider style={{ marginBottom: 32 }} />
-        <div className={styles.title}>详情列表1</div>
-        {basisDetailData && basisDetailData.basicGoods ? (
-          <DetailList1 basicGoods={basisDetailData.basicGoods} loading={loading} />
-        ) : null}
-        <div className={styles.title}>详情列表2</div>
-        {basisDetailData && basisDetailData.basicProgress ? (
-          <DetailList2 basicProgress={basisDetailData.basicProgress} loading={loading} />
-        ) : null}
+        {basisDetailData ? (
+          <Fragment>
+            <BasisDetail1 data={basisDetailData.basisDescriptionData1} loading={loading} />
+            <Divider style={{ marginBottom: 32 }} />
+            <BasisDetail2 data={basisDetailData.basisDescriptionData2} loading={loading} />
+            <Divider style={{ marginBottom: 32 }} />
+            <div className={styles.title}>详情列表1</div>
+            <DetailList1 basicGoods={basisDetailData.basicGoods} loading={loading} />
+            <div className={styles.title}>详情列表2</div>
+            <DetailList2 basicProgress={basisDetailData.basicProgress} loading={loading} />
+          </Fragment>
+        ) : (
+          <ProSkeleton type="descriptions" pageHeader={false} />
+        )}
       </Card>
     </PageContainer>
   );
