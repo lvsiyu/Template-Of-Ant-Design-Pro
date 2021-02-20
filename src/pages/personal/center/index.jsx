bd4495d292c0b92bf0c928a122564ffd2a0ef5f0
lvsiyu <1030502996@qq.com>
2021-02-20 17:47:31 +0800

    '用户中心框架搭建完成'

--- src/pages/personal/center/index.jsx
+++ src/pages/personal/center/index.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Row, Col } from 'antd';
 import { PageContainer } from '@ant-design/pro-layout';
+import ProCard from '@ant-design/pro-card';
 
 const UserCenter = () => {
-  return <PageContainer />;
+  const [tab, setTab] = useState('tab1');
+  return (
+    <PageContainer>
+      <Row gutter={[16, 0]}>
+        <Col span={8}>
+          <ProCard title="111">11111</ProCard>
+        </Col>
+        <Col span={16}>
+          <ProCard
+            tabs={{
+              tabPosition: 'top',
+              activeKey: tab,
+              onChange: (key) => {
+                setTab(key);
+              },
+            }}
+          >
+            <ProCard.TabPane key="tab1" tab="内容一">
+              内容一
+            </ProCard.TabPane>
+            <ProCard.TabPane key="tab2" tab="内容二">
+              内容二
+            </ProCard.TabPane>
+          </ProCard>
+        </Col>
+      </Row>
+    </PageContainer>
+  );
 };
 
 export default UserCenter;
--- src/pages/personal/user/index.jsx
+++ src/pages/personal/user/index.jsx
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
+import ProCard from '@ant-design/pro-card';
 
 const UserInfo = () => {
-  return <PageContainer />;
+  const [tab, setTab] = useState('tab1');
+  return (
+    <PageContainer>
+      <ProCard
+        tabs={{
+          tabPosition: 'left',
+          activeKey: tab,
+          onChange: (key) => {
+            setTab(key);
+          },
+        }}
+      >
+        <ProCard.TabPane key="tab1" tab="基本设置">
+          <ProCard direction="column" gutter={[0, 16]}>
+            <ProCard bordered>内容1-1</ProCard>
+            <ProCard bordered>内容1-2</ProCard>
+          </ProCard>
+        </ProCard.TabPane>
+        <ProCard.TabPane key="tab2" tab="安全设置">
+          <ProCard direction="column" gutter={[0, 16]}>
+            <ProCard bordered>内容2-1</ProCard>
+            <ProCard bordered>内容2-2</ProCard>
+          </ProCard>
+        </ProCard.TabPane>
+        <ProCard.TabPane key="tab3" tab="新消息通知">
+          <ProCard direction="column" gutter={[0, 16]}>
+            <ProCard bordered>内容3-1</ProCard>
+            <ProCard bordered>内容3-2</ProCard>
+          </ProCard>
+        </ProCard.TabPane>
+      </ProCard>
+    </PageContainer>
+  );
 };
 
 export default UserInfo;
